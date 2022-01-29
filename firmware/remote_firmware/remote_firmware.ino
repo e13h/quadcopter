810f8a61af4ebc803983f7dec9323d40e21a32de
Evan Smith <evanphilipsmith@gmail.com>
2022-01-29 13:13:26 -0800

    Starter code

---
+++ firmware/remote_firmware/remote_firmware.ino
@@ -0,0 +1,15 @@
+#include <radio.h>
+#include "quad_remote.h"  // Header file with pin definitions and setup
+#include <serLCD.h>
+
+SerLCD remoteLCD;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
