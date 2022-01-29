14bb58a27530ea01f7e0de1e8ba2083ffb5eff51
Evan Smith <evanphilipsmith@gmail.com>
2022-01-29 15:35:54 -0800

    Code for reading the gimbals and printing to serial

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -2,14 +2,33 @@
 #include "quad_remote.h"  // Header file with pin definitions and setup
 #include <serLCD.h>
 
-SerLCD remoteLCD;
-
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int throttle = analogRead(PIN_THROTTLE);
+  throttle = map(throttle, 0, 1023, 0, 255);
+  Serial.print(throttle);
+  Serial.print(" ");
 
+  int yaw = analogRead(PIN_YAW);
+  yaw = map(yaw, 0, 1023, 0, 255);
+  Serial.print(yaw);
+  Serial.print(" ");
+  
+  int roll = analogRead(PIN_ROLL);
+  roll = map(roll, 0, 1023, 0, 255);
+  Serial.print(roll);
+  Serial.print(" ");
+
+  int pitch = analogRead(PIN_PITCH);
+  pitch = map(pitch, 0, 1023, 0, 255);
+  Serial.print(pitch);
+  Serial.print(" ");
+  Serial.print("\n");
+  delay(10);
 }
